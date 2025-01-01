6cb3e1429f96c467b751109ac01a0a55dd8e04ac
Avinash <akgpcamp@gmail.com>
2025-01-01 10:24:14 +0530

    ApnaCollege

--- Sorting/Main.java
+++ Sorting/Main.java
@@ -3,11 +3,29 @@ package Sorting;
 public class Main {
     public static void main(String[] args) {
         int[] arr = {5, 4, 1, 3, 2};
-        bubbleSort(arr);
-        selectionSort(arr);
+//        bubbleSort(arr);
+//        selectionSort(arr);
+        insertionSort(arr);
         printArr(arr);
     }
 
+    //    O(n^2)
+    public static void insertionSort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            int curr = arr[i];
+            int prev = i - 1;
+//            finding out the correct position to insert
+            while (prev >= 0 && arr[prev] > curr) {
+                arr[prev + 1] = arr[prev];
+                prev--;
+            }
+//            insertion
+            arr[prev + 1] = curr;
+
+        }
+    }
+
+    //  O(n^2)
     public static void selectionSort(int[] arr) {
         for (int i = 0; i < arr.length - 1; i++) {
             int minPos = i;
