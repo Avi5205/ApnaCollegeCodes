77344e09d9869818c5e31a48321f16fd4b6ea14c
Avinash <akgpcamp@gmail.com>
2025-01-01 10:36:42 +0530

    ApnaCollege

--- Sorting/Main.java
+++ Sorting/Main.java
@@ -5,8 +5,29 @@ public class Main {
         int[] arr = {5, 4, 1, 3, 2};
 //        bubbleSort(arr);
 //        selectionSort(arr);
-        insertionSort(arr);
+//        insertionSort(arr);
+        countingSort(arr);
         printArr(arr);
+    }
+
+    public static void countingSort(int[] arr) {
+        int largest = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            largest = Math.max(largest, arr[i]);
+        }
+        int[] count = new int[largest + 1];
+        for (int i = 0; i < arr.length; i++) {
+            count[arr[i]]++;
+        }
+
+        int j = 0;
+        for (int i = 0; i < count.length; i++) {
+            while (count[i] > 0) {
+                arr[j] = i;
+                j++;
+                count[i]--;
+            }
+        }
     }
 
     //    O(n^2)
