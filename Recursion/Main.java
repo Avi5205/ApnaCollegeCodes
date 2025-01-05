21721fcac1c82497b5fafef8c7ada299a335eaba
Avinash <akgpcamp@gmail.com>
2025-01-05 18:37:59 +0530

    ApnaCollege Codes

--- Recursion/Main.java
+++ Recursion/Main.java
@@ -2,10 +2,29 @@ package Recursion;
 
 public class Main {
     public static void main(String[] args) {
-        int n = 10;
+        int n = 25;
         printDec(n);
         printAsc(n);
         System.out.println("\nFactorial of " + n + " is " + findFactorial(n));
+        System.out.println("Sum of Natural number:" + n + " = " + sumOfNaturalNumber(n));
+        System.out.println("Fibonacci of " + n + " is " + fib(n));
+
+
+    }
+
+    //  sum of N fibonacci series
+    public static int fib(int n) {
+        if (n == 0 || n == 1) {
+            return n;
+        }
+        return fib(n - 1) + fib(n - 2);
+    }
+
+    public static int sumOfNaturalNumber(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        return n + sumOfNaturalNumber(n - 1);
     }
 
     public static int findFactorial(int n) {
